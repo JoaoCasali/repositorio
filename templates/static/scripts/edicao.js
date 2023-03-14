22e6149717cf956d87e71b53f267584a9aa118d5
João Vitor <joaovitorcasali@gmail.com>
2023-03-13 21:20:38 -0300

    Arrumando cadastro de comentário

--- templates/static/scripts/edicao.js
+++ templates/static/scripts/edicao.js
@@ -3,14 +3,14 @@ $(document).ready(function () {
 
     $('.summernote.editavel').summernote({
         toolbar: [
-            // [groupName, [list of button]]
             ['style', ['bold', 'italic', 'underline', 'clear']],
             ['font', ['strikethrough', 'superscript', 'subscript']],
             ['fontsize', ['fontsize']],
             ['color', ['color']],
             ['para', ['ul', 'ol', 'paragraph']],
             ['height', ['height']]
-        ]
+        ],
+        height: 300
     });
 
     $('.summernote.listagem').summernote({
